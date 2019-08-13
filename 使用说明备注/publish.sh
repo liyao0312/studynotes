4c0f317de4fa4c56ad448f34834381a25748fa6a
liyao0312 <370582412@qq.com>
2019-08-13 22:40:42 +0800

    publish gh-pages

---
+++ 使用说明备注/publish.sh
@@ -0,0 +1,17 @@
+cd /f/Leo/StudyNotes  &&\
+git checkout master &&\
+gitbook init &&\
+gitbook build &&\
+git add . &&\
+git commit -m 'update gitbook' &&\
+git push origin master &&\
+git checkout gh-pages &&\
+rm -rf * &&\
+git checkout master -- _book &&\
+mv _book/* ./ &&\
+rm -rf _book &&\
+rm -rf publish.sh &&\
+git add . &&\
+git commit -m 'publish gh-pages' &&\
+git push origin gh-pages &&\
+git checkout master
